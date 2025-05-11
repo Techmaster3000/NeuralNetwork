85e418e9b067c5a361cc0ff4d9b8906b17b07033
Techmaster3000 <52240882+Techmaster3000@users.noreply.github.com>
2025-05-11 18:12:22 +0200

    tried and failed tensorflow import

--- tensorflow.py
+++
@@ -1,8 +0,0 @@
-import numpy as np 
-import pandas as pd 
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
